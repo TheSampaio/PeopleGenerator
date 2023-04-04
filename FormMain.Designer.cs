c3f930fec2f84147639a5e3389b1976f2da66ad8
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2023-04-03 21:50:32 -0300

    Initial commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace PeopleGenerator
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_Generate = new Button();
+            txt_Name = new TextBox();
+            label1 = new Label();
+            txt_Occupation = new TextBox();
+            label2 = new Label();
+            txt_Income = new TextBox();
+            label3 = new Label();
+            txt_Age = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            txt_Birthday = new TextBox();
+            txt_MaritalStatus = new TextBox();
+            label6 = new Label();
+            rdo_Male = new RadioButton();
+            rdo_Female = new RadioButton();
+            SuspendLayout();
+            // 
+            // btn_Generate
+            // 
+            btn_Generate.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_Generate.Location = new Point(354, 481);
+            btn_Generate.Name = "btn_Generate";
+            btn_Generate.Size = new Size(75, 32);
+            btn_Generate.TabIndex = 0;
+            btn_Generate.Text = "Generate";
+            btn_Generate.UseVisualStyleBackColor = true;
+            btn_Generate.Click += btn_Generate_Click;
+            // 
+            // txt_Name
+            // 
+            txt_Name.BackColor = Color.White;
+            txt_Name.BorderStyle = BorderStyle.FixedSingle;
+            txt_Name.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_Name.Location = new Point(463, 106);
+            txt_Name.Margin = new Padding(4);
+            txt_Name.MaxLength = 30;
+            txt_Name.Name = "txt_Name";
+            txt_Name.ReadOnly = true;
+            txt_Name.Size = new Size(233, 25);
+            txt_Name.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(399, 116);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name: ";
+            // 
+            // txt_Occupation
+            // 
+            txt_Occupation.BackColor = Color.White;
+            txt_Occupation.BorderStyle = BorderStyle.FixedSingle;
+            txt_Occupation.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_Occupation.Location = new Point(481, 251);
+            txt_Occupation.Margin = new Padding(4);
+            txt_Occupation.MaxLength = 30;
+            txt_Occupation.Name = "txt_Occupation";
+            txt_Occupation.ReadOnly = true;
+            txt_Occupation.Size = new Size(215, 25);
+            txt_Occupation.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(399, 261);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Occupation: ";
+            // 
+            // txt_Income
+            // 
+            txt_Income.BackColor = Color.White;
+            txt_Income.BorderStyle = BorderStyle.FixedSingle;
+            txt_Income.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_Income.Location = new Point(481, 284);
+            txt_Income.Margin = new Padding(4);
+            txt_Income.MaxLength = 30;
+            txt_Income.Name = "txt_Income";
+            txt_Income.ReadOnly = true;
+            txt_Income.Size = new Size(107, 25);
+            txt_Income.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(399, 294);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Income: ";
+            // 
+            // txt_Age
+            // 
+            txt_Age.BackColor = Color.White;
+            txt_Age.BorderStyle = BorderStyle.FixedSingle;
+            txt_Age.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_Age.Location = new Point(653, 139);
+            txt_Age.Margin = new Padding(4);
+            txt_Age.MaxLength = 30;
+            txt_Age.Name = "txt_Age";
+            txt_Age.ReadOnly = true;
+            txt_Age.Size = new Size(43, 25);
+            txt_Age.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(612, 149);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Age: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(399, 149);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 15);
+            label5.TabIndex = 2;
+            label5.Text = "Birthday: ";
+            // 
+            // txt_Birthday
+            // 
+            txt_Birthday.BackColor = Color.White;
+            txt_Birthday.BorderStyle = BorderStyle.FixedSingle;
+            txt_Birthday.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_Birthday.Location = new Point(463, 139);
+            txt_Birthday.Margin = new Padding(4);
+            txt_Birthday.MaxLength = 30;
+            txt_Birthday.Name = "txt_Birthday";
+            txt_Birthday.ReadOnly = true;
+            txt_Birthday.Size = new Size(125, 25);
+            txt_Birthday.TabIndex = 1;
+            // 
+            // txt_MaritalStatus
+            // 
+            txt_MaritalStatus.BackColor = Color.White;
+            txt_MaritalStatus.BorderStyle = BorderStyle.FixedSingle;
+            txt_MaritalStatus.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_MaritalStatus.Location = new Point(491, 172);
+            txt_MaritalStatus.Margin = new Padding(4);
+            txt_MaritalStatus.MaxLength = 30;
+            txt_MaritalStatus.Name = "txt_MaritalStatus";
+            txt_MaritalStatus.ReadOnly = true;
+            txt_MaritalStatus.Size = new Size(97, 25);
+            txt_MaritalStatus.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(399, 182);
+            label6.Name = "label6";
+            label6.Size = new Size(85, 15);
+            label6.TabIndex = 2;
+            label6.Text = "Marital Status: ";
+            // 
+            // rdo_Male
+            // 
+            rdo_Male.AutoSize = true;
+            rdo_Male.Checked = true;
+            rdo_Male.Location = new Point(399, 363);
+            rdo_Male.Name = "rdo_Male";
+            rdo_Male.Size = new Size(51, 19);
+            rdo_Male.TabIndex = 3;
+            rdo_Male.TabStop = true;
+            rdo_Male.Text = "Male";
+            rdo_Male.UseVisualStyleBackColor = true;
+            // 
+            // rdo_Female
+            // 
+            rdo_Female.AutoSize = true;
+            rdo_Female.Location = new Point(399, 388);
+            rdo_Female.Name = "rdo_Female";
+            rdo_Female.Size = new Size(63, 19);
+            rdo_Female.TabIndex = 3;
+            rdo_Female.Text = "Female";
+            rdo_Female.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            ClientSize = new Size(784, 561);
+            Controls.Add(rdo_Female);
+            Controls.Add(rdo_Male);
+            Controls.Add(label4);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txt_MaritalStatus);
+            Controls.Add(txt_Birthday);
+            Controls.Add(txt_Age);
+            Controls.Add(txt_Income);
+            Controls.Add(txt_Occupation);
+            Controls.Add(txt_Name);
+            Controls.Add(btn_Generate);
+            MaximizeBox = false;
+            Name = "FormMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "People Generator";
+            TopMost = true;
+            FormClosing += FormMain_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_Generate;
+        private TextBox txt_Name;
+        private Label label1;
+        private TextBox txt_Occupation;
+        private Label label2;
+        private TextBox txt_Income;
+        private Label label3;
+        private TextBox txt_Age;
+        private Label label4;
+        private Label label5;
+        private TextBox txt_Birthday;
+        private TextBox txt_MaritalStatus;
+        private Label label6;
+        private RadioButton rdo_Male;
+        private RadioButton rdo_Female;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,285 @@
+using Microsoft.VisualBasic.Devices;
+using System.Diagnostics;
+using System.Reflection;
+
+// Store an occupation with name and salary
+struct Occupation
+{
+    // Attributes
+    public readonly string Name;
+    public readonly string Salary;
+
+    // Constructor
+    public Occupation(string Name, string Salary)
+    {
+        this.Name = Name;
+        this.Salary = Salary;
+    }
+}
+
+namespace PeopleGenerator
+{
+    public partial class FormMain : Form
+    {
+        // Attributes
+        Random Random = new Random();
+
+        List<string> MaleNames = new List<string>();
+        List<string> FemaleNames = new List<string>();
+
+        List<string> Surnames = new List<string>();
+        List<Occupation> Occupations = new List<Occupation>();
+
+        string TemporaryName = "None";
+
+        // Contructor
+        public FormMain()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            // Add male names
+            MaleNames.Add("Arthur");
+            MaleNames.Add("Ducth");
+            MaleNames.Add("Michael");
+            MaleNames.Add("Franklin");
+            MaleNames.Add("Trevor");
+            MaleNames.Add("Steve");
+            MaleNames.Add("John");
+            MaleNames.Add("Paul");
+            MaleNames.Add("Dave");
+            MaleNames.Add("Ethan");
+            MaleNames.Add("Chris");
+            MaleNames.Add("Julius");
+            MaleNames.Add("Sean");
+            MaleNames.Add("Matthew");
+            MaleNames.Add("Charlie");
+            MaleNames.Add("Dean");
+            MaleNames.Add("Samuel");
+            MaleNames.Add("Jason");
+            MaleNames.Add("Jakson");
+
+            // Add female names
+            FemaleNames.Add("Ada");
+            FemaleNames.Add("Claire");
+            FemaleNames.Add("Lara");
+            FemaleNames.Add("Laura");
+            FemaleNames.Add("Mary");
+            FemaleNames.Add("Hellena");
+            FemaleNames.Add("Sophia");
+            FemaleNames.Add("Abigail");
+            FemaleNames.Add("Molly");
+            FemaleNames.Add("Olivia");
+            FemaleNames.Add("Isla");
+            FemaleNames.Add("Mia");
+            FemaleNames.Add("Lily");
+            FemaleNames.Add("Isabela");
+            FemaleNames.Add("Amelia");
+            FemaleNames.Add("Ava");
+            FemaleNames.Add("Emily");
+            FemaleNames.Add("Grace");
+            FemaleNames.Add("Lucy");
+            FemaleNames.Add("Freya");
+            FemaleNames.Add("Amber");
+            FemaleNames.Add("Bonnie");
+            FemaleNames.Add("Beatrice");
+            FemaleNames.Add("Saldina");
+            FemaleNames.Add("Florence");
+            FemaleNames.Add("Martha");
+            FemaleNames.Add("Darcie");
+            FemaleNames.Add("Nancy");
+            FemaleNames.Add("Arabella");
+            FemaleNames.Add("Saskia");
+            FemaleNames.Add("Rosanna");
+            FemaleNames.Add("Anna");
+            FemaleNames.Add("Maryam");
+            FemaleNames.Add("Dolly");
+            FemaleNames.Add("Hettie");
+            FemaleNames.Add("Libby");
+            FemaleNames.Add("Sadie");
+            FemaleNames.Add("Katherine");
+            FemaleNames.Add("Adele");
+            FemaleNames.Add("Connery");
+            FemaleNames.Add("Aila");
+            FemaleNames.Add("Mirren");
+            FemaleNames.Add("Gwen");
+            FemaleNames.Add("Ella");
+            FemaleNames.Add("Ivy");
+            FemaleNames.Add("Rosie");
+            FemaleNames.Add("Rose");
+            FemaleNames.Add("Megan");
+            FemaleNames.Add("Mia");
+            FemaleNames.Add("Merryn");
+            FemaleNames.Add("Sabrina");
+            FemaleNames.Add("Bristol");
+            FemaleNames.Add("Kenza");
+            FemaleNames.Add("Kezia");
+            FemaleNames.Add("Serana");
+            FemaleNames.Add("Jane");
+            FemaleNames.Add("Victoria");
+            FemaleNames.Add("Diana");
+            FemaleNames.Add("Elizabeth");
+            FemaleNames.Add("Margaret");
+
+            // Add surnames
+            Surnames.Add("Scott");
+            Surnames.Add("Burton");
+            Surnames.Add("Adams");
+            Surnames.Add("Harris");
+            Surnames.Add("Wilson");
+            Surnames.Add("Baker");
+            Surnames.Add("Payne");
+            Surnames.Add("Walker");
+            Surnames.Add("Owen");
+            Surnames.Add("Holmes");
+            Surnames.Add("Lewis");
+            Surnames.Add("Robinson");
+            Surnames.Add("Allen");
+            Surnames.Add("Jones");
+            Surnames.Add("Foster");
+            Surnames.Add("Matthews");
+            Surnames.Add("White");
+            Surnames.Add("Stevens");
+            Surnames.Add("Griffiths");
+            Surnames.Add("Evans");
+            Surnames.Add("Young");
+            Surnames.Add("Corbyn");
+            Surnames.Add("Smith");
+            Surnames.Add("Evans");
+            Surnames.Add("Jenkins");
+            Surnames.Add("Green");
+            Surnames.Add("Hughes");
+            Surnames.Add("Taylor");
+            Surnames.Add("Hall");
+            Surnames.Add("Anderson");
+            Surnames.Add("Armstrong");
+            Surnames.Add("Bell");
+            Surnames.Add("Cox");
+            Surnames.Add("Cartex");
+            Surnames.Add("Cole");
+            Surnames.Add("Collins");
+            Surnames.Add("Atkinson");
+            Surnames.Add("Dawson");
+            Surnames.Add("Bailey");
+            Surnames.Add("Ball");
+            Surnames.Add("Dixon");
+            Surnames.Add("Edwards");
+            Surnames.Add("Browns");
+            Surnames.Add("Clarke");
+            Surnames.Add("Yates");
+            Surnames.Add("Zelly");
+            Surnames.Add("Zouch");
+            Surnames.Add("Fisher");
+            Surnames.Add("Ford");
+            Surnames.Add("Fletcher");
+            Surnames.Add("Fox");
+            Surnames.Add("Gibson");
+            Surnames.Add("Graham");
+            Surnames.Add("Grant");
+            Surnames.Add("Gray");
+            Surnames.Add("Sallow");
+            Surnames.Add("Fernsby");
+            Surnames.Add("Villin");
+            Surnames.Add("Miracle");
+            Surnames.Add("Dankworth");
+            Surnames.Add("Relish");
+            Surnames.Add("Palmer");
+            Surnames.Add("Parker");
+            Surnames.Add("Quintrell");
+            Surnames.Add("Quill");
+            Surnames.Add("Stewart");
+            Surnames.Add("Turner");
+            Surnames.Add("Walsh");
+            Surnames.Add("Cooper");
+            Surnames.Add("Morris");
+            Surnames.Add("Harrison");
+            Surnames.Add("Morgan");
+            Surnames.Add("Watson");
+            Surnames.Add("Campbell");
+            Surnames.Add("Ainsworth");
+            Surnames.Add("Eaton");
+            Surnames.Add("Elton");
+            Surnames.Add("Camden");
+            Surnames.Add("Browning");
+            Surnames.Add("Garfield");
+            Surnames.Add("Dalton");
+            Surnames.Add("Cobain");
+            Surnames.Add("Duke");
+            Surnames.Add("Bobellon");
+            Surnames.Add("Carmichael");
+            Surnames.Add("Granger");
+            Surnames.Add("Kennedy");
+            Surnames.Add("Lawrence");
+            Surnames.Add("Halifax");
+            Surnames.Add("Lloyd");
+            Surnames.Add("Osborne");
+            Surnames.Add("Marshall");
+
+            // Add occupations
+            Occupations.Add(new Occupation("Architect", "46,883.21"));
+            Occupations.Add(new Occupation("Baker", "25,506.46"));
+            Occupations.Add(new Occupation("Cleaner", "21,860.13"));
+            Occupations.Add(new Occupation("Dentist", "97,002.82"));
+            Occupations.Add(new Occupation("Doctor", "121,308.79"));
+        }
+
+        private string RandomName(bool Male)
+        {
+            // Return a random name
+            return (Male) ? MaleNames.ElementAt(Random.Next(0, MaleNames.Count)) : FemaleNames.ElementAt(Random.Next(0, FemaleNames.Count));
+        }
+
+        private string RandomSurname()
+        {
+            // Store a random surname
+            string Surname = Surnames.ElementAt(Random.Next(0, Surnames.Count));
+
+            // Store the last surname choused
+            if (TemporaryName != "None")
+                Surnames.Add(TemporaryName);
+
+            // Store surname in a local variable and delete it from surname list
+            TemporaryName = Surname;
+            Surnames.RemoveAt(Surnames.IndexOf(Surname));
+            return Surname;
+        }
+
+        private Occupation RandomOccupation()
+        {
+            // Return a random occupation with a salary linked
+            return Occupations.ElementAt(Random.Next(0, Occupations.Count));
+        }
+
+        private void btn_Generate_Click(object sender, EventArgs e)
+        {
+            // Choose between male and female and randomize a complete name
+            txt_Name.Text = RandomName(rdo_Male.Checked) + " " + RandomSurname() + " " + RandomSurname();
+
+            // Calculates age and birthday date
+            short Age = (short)Random.Next(18, 60);
+            txt_Age.Text = Convert.ToString(Age);
+            txt_Birthday.Text = Convert.ToString(Random.Next(1, 28)) + "/" + Convert.ToString(Random.Next(1, 12)) + "/" + Convert.ToString(DateTime.Now.Year - Age);
+
+            // Choose between single and married 
+            txt_MaritalStatus.Text = (Random.Next(0, 9) <= 4) ? "Single" : "Married";
+
+            // Randomize an occupation and get its salary per year
+            Occupation OccupationCache = RandomOccupation();
+            txt_Occupation.Text = OccupationCache.Name;
+            txt_Income.Text = "£ " + OccupationCache.Salary;
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Show warning message box
+            DialogResult Result = MessageBox.Show("Do you really want to close the application?",
+                "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+
+            // Verify if user want to close the window
+            e.Cancel = (Result == DialogResult.No) ? true : false;
+        }
+    }
+}
